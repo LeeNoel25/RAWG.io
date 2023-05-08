9c66727b79e5936220c958976ce9c9ad0521fdaf
LeeNoel <112954324+LeeNoel25@users.noreply.github.com>
2023-05-08 18:52:03 +0800

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,13 @@
 import useGenres from '../hooks/useGenres';
-import { List, ListItem, HStack, Image, Text, Spinner } from '@chakra-ui/react';
+import { List, ListItem, HStack, Image, Spinner, Button } from '@chakra-ui/react';
 import getCroppedImageUrl from '../services/image-url';
+import { Genre } from '../hooks/useGenres';
 
-const GenreList = () => {
+interface Props {
+    onSelectGenre: (genre: Genre) => void
+}
+
+const GenreList = ({onSelectGenre}: Props) => {
   const { data, isLoading, error } = useGenres();
     if (isLoading) return <Spinner />;
     if (error) return null
@@ -13,7 +18,7 @@ const GenreList = () => {
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-            <Text fontSize='lg'>{genre.name}</Text>
+            <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
